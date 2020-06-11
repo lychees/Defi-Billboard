9a81af8e35c107e4049c2168dc512e76f6513ed9
lychees <lychees67@gmail.com>
2020-06-10 21:23:41 -0700

    Fix a bug

---
+++ fe/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "parserOptions": {
+    "parser": "babel-eslint"
+  },
+  "extends": [
+    "plugin:vue/recommended"
+  ],
+  "rules": {
+    "no-console": "off",
+  },
+  "globals": {
+    "ethereum": true,
+    "web3": true,
+  },
+};    
